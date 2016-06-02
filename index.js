69873b540745d9465a093edb7916bcc3eff48924
Tom Loudon <tomloudon@gmail.com>
2016-06-02 07:42:11 +0100

    Removed comment about possible API formats

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
 export { default as draw } from './src/patterns';
-
-// // possible API formats
-//
-// // non-chained
-// pattern.format('svg');
-// pattern.draw('circle', '#333');
-//
-// // chained
-// pattern
-//   .format('svg')
-//   .draw('circle', '#333');
-
-// // default is cavas
-// pattern
-//   .draw('circle', '#333');
