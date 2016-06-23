72e0435a90838c03827c5cc5a5ccb9d5aac9a9cb
Tom Loudon <tomloudon@gmail.com>
2016-06-23 13:25:36 +0100

    Added test for pattern module

---
+++ test/patterns.js
@@ -0,0 +1,16 @@
+import test from 'tape';
+import canvas from 'canvas';
+import jsdom from 'jsdom';
+
+import pattern from '../src/patterns';
+
+global.document = jsdom.jsdom('<html></html>')
+global.window = global.document.defaultView;
+
+test('pattern returns a canvas context', (t) => {
+  t.plan(1);
+
+  let testPattern = pattern();
+
+  t.equal(testPattern.toString(), '[object CanvasPattern]');
+});
