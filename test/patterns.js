1690c78d3a4a9cd87a2f766f9b3e770a107dd5ef
Tom Loudon <tomloudon@gmail.com>
2016-06-28 22:16:40 +0100

    Reworded test case and removed unnecessary jsdom code

--- test/patterns.js
+++ test/patterns.js
@@ -1,12 +1,8 @@
 import test from 'tape';
-import jsdom from 'jsdom';
 
 import pattern from '../src/patterns';
 
-global.document = jsdom.jsdom('<html></html>');
-global.window = global.document.defaultView;
-
-test('pattern returns a canvas context', (t) => {
+test('pattern returns a canvas pattern', (t) => {
   t.plan(1);
 
   let testPattern = pattern();
