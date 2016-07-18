a81c355b87b126e996b073c26b65b25e8b0aa79a
Tom Loudon <tomloudon@gmail.com>
2016-07-18 12:34:44 +0100

    Removed ratio from calculations as it doesn't help

--- src/patterns.js
+++ src/patterns.js
@@ -8,12 +8,11 @@ function draw (
   shape = 'square',
   backgroundColor = 'rgba(100, 100, 100, 0.7)',
   patternColor = 'rgba(255, 255, 255, 0.7)',
-  size = 8
+  size = 10
 ) {
   let outerSize = size * 2;
   let patternCanvas = document.createElement('canvas');
   let patternContext = patternCanvas.getContext('2d');
-  let ratio = window.devicePixelRatio;
   let pattern;
 
   switch (shape) {
@@ -35,12 +34,11 @@ function draw (
   patternContext.fillStyle = backgroundColor;
   patternContext.fillRect(0, 0, patternCanvas.width, patternCanvas.height);
 
-  patternContext.scale(1 / ratio, 1 / ratio);
-
   // create pattern overlay rect
-  pattern = patternContext.createPattern(pattern.call(this, size * ratio), 'repeat');
+  pattern = patternContext.createPattern(pattern.call(this, size), 'repeat');
   patternContext.fillStyle = pattern;
-  patternContext.fillRect(0, 0, size * ratio, size * ratio);
+  patternContext.fillRect(0, 0, size, size);
+
 
   return patternContext.createPattern(patternCanvas, 'repeat');
 }
