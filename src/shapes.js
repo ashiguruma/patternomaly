a86767dcc664fd893868e243dc92868091ecf0c2
Tom Loudon <tomloudon@gmail.com>
2016-07-28 18:04:02 +0100

    Updated some shapes to form improved tiled patterns

--- src/shapes.js
+++ src/shapes.js
@@ -17,8 +17,9 @@ export function square (width) {
   let shape = generateShape(width);
   let height = width;
 
-  shape.context.fillStyle = 'rgba(255, 255, 255, 0.7)';
+  shape.context.fillStyle = 'rgba(255, 255, 255, 0.4)';
   shape.context.fillRect(0, 0, width / 2, height / 2);
+  shape.context.fillRect(width / 2, height / 2, width / 2, height / 2);
 
   return shape.canvas;
 }
@@ -30,8 +31,10 @@ export function circle (diameter) {
   let offset = radius;
 
   shape.context.beginPath();
-  shape.context.fillStyle = 'rgba(255, 255, 255, 0.7)';
-  shape.context.arc(offset, offset, radius / 2, 0, 2 * Math.PI);
+  shape.context.fillStyle = 'rgba(255, 255, 255, 0.4)';
+
+  shape.context.arc(radius / 2, radius / 2, radius / 2, 0, 2 * Math.PI);
+  shape.context.arc(radius * 1.5, radius * 1.5, radius / 2, 0, 2 * Math.PI);
   shape.context.fill();
 
   return shape.canvas;
@@ -39,7 +42,7 @@ export function circle (diameter) {
 
 export function diamond (width) {
   let shape = generateShape(width);
-  width = Math.sqrt(Math.pow(width, 2) / 2) / 2;
+  width = Math.sqrt(Math.pow(width, 2) / 2) / 1.5;
   let height = width;
 
   shape.context.translate(shape.canvas.width / 2, shape.canvas.height / 2);
@@ -122,11 +125,33 @@ export function triangle (size) {
   size = size / 2;
 
   shape.context.beginPath();
-  shape.context.fillStyle = 'rgba(255, 255, 255, 0.7)';
-  shape.context.moveTo(size, size * 0.5);
-  shape.context.lineTo(size * 1.5, size * 1.5);
-  shape.context.lineTo(size * 0.5, size * 1.5);
-  shape.context.lineTo(size, size * 0.5);
+  shape.context.fillStyle = 'rgba(255, 255, 255, 0.5)';
+
+  shape.context.moveTo(size / 2, 0);
+  shape.context.lineTo(size, size);
+  shape.context.lineTo(0, size);
+  shape.context.lineTo(size / 2, 0);
+
+  shape.context.moveTo(size, size);
+  shape.context.lineTo(size * 1.5, 0);
+  shape.context.lineTo(size * 2, size);
+  shape.context.lineTo(size, size);
+
+  shape.context.moveTo(size * 2, size);
+  shape.context.lineTo(size * 2, size * 2);
+  shape.context.lineTo(size * 1.5, size * 2);
+  shape.context.lineTo(size * 2, size);
+
+  shape.context.moveTo(size, size);
+  shape.context.lineTo(size * 1.5, size * 2);
+  shape.context.lineTo(size / 2, size * 2);
+  shape.context.lineTo(size, size);
+
+  shape.context.moveTo(0, size);
+  shape.context.lineTo(size / 2, size * 2);
+  shape.context.lineTo(0, size * 2);
+  shape.context.lineTo(0, size);
+
   shape.context.fill();
 
   return shape.canvas;
