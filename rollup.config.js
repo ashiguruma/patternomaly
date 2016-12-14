80fd8127cecad71d4a364689202cdf7d69ddc194
Tom Loudon <tomloudon@gmail.com>
2016-12-14 18:04:07 +0000

    Added code coverage
    Added coverage using istanbul. Also cleaned up a few bits

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel';
 import babelrc from 'babelrc-rollup';
 
-let pkg = require('./package.json');
+const pkg = require('./package.json');
 
 export default {
   entry: 'index.js',
